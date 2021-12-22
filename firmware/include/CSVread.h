75df95377e2cced446caa63650b1b77a7086ee02
Hara <36100321+hrjp@users.noreply.github.com>
2021-12-22 11:51:47 +0900

    sd read add

---
+++ firmware/include/CSVread.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <SdFat.h>
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+class CSVread{
+public:
+    CSVread();
+    CSVread(const std::string& file_path);
+    bool getCSV(const std::string& file_path);
+    void print();
+    std::vector<std::vector<std::string> > data;
+private:
+    SdFs sd;
+    FsFile file;
+    
+};
+
+CSVread::CSVread():sd(),file(){
+
+}
+
+CSVread::CSVread(const std::string& file_path):sd(),file(){
+    getCSV(file_path);
+}
+
+bool CSVread::getCSV(const std::string& file_path){
+    if (!sd.begin(SdioConfig(FIFO_SDIO))) {
+        sd.initErrorHalt(&Serial);
+        return false;
+    }
+    if (!file.open("202112.csv", FILE_WRITE)) {
+        Serial.println("file open failed");
+        return false;
+    }
+    else{
+        Serial.println("file open successful");
+    }
+    data.resize(1);
+    data[0].resize(0);
+    char line[1000];
+    int i = 0;
+    int j = 0;
+    file.rewind();
+    while (file.available()) {
+        file.fgets(line, sizeof(line));
+        std::string tmp = "";
+        std::string str(line);
+        std::istringstream stream(str);
+        // 区切り文字がなくなるまで文字を区切っていく
+        while (getline(stream, tmp, ',')){
+            data.at(i).push_back(tmp);
+            j++;
+        }
+        j = 0;
+        i++;  
+        data.resize(i+1);
+        //Serial.print(line);
+    }
+    file.close();
+    return true;
+}
+
+void CSVread::print(){
+    bool sepalator_skip=true;
+    for(auto k:data){
+        for(auto l:k){
+            if(!sepalator_skip){
+                Serial.print(",");
+            }
+            sepalator_skip=false;
+            Serial.print(l.c_str());
+        }
+        sepalator_skip=true;
+    }
+}
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,10 +1,36 @@
 #include <Arduino.h>
 #include <TinyGPS++.h>
+#include <SdFat.h>
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "CSVread.h"
+
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+
+CSVread csv("202112.csv");
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  Serial.begin(115200);
+  Serial1.begin(GPSBaud);
+
+  csv.print();
+  //Serial.print(csv.data[0][1].c_str());
 }
 
 void loop() {
+
+  while (Serial1.available()){
+      gps.encode(Serial1.read());
+  }
+  char sz[128];
+  sprintf(sz, "%02d:%02d:%02d:%02d:%02d:%02d ", gps.date.year(),gps.date.month(), gps.date.day(),gps.time.hour(), gps.time.minute(), gps.time.second());
+  //Serial.println(sz);
   // put your main code here, to run repeatedly:
 }
